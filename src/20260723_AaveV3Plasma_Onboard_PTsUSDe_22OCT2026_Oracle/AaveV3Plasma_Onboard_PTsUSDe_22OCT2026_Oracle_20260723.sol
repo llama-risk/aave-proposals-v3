e48bebecb76e11cbf960728580b167eb5d922a52
Rare Weasel <rareweasel@protonmail.com>
2026-07-24 11:02:34 -0600

    feat: update PTsUSDe22OCT2026

--- src/20260723_AaveV3Plasma_Onboard_PTsUSDe_22OCT2026_Oracle/AaveV3Plasma_Onboard_PTsUSDe_22OCT2026_Oracle_20260723.sol
+++ src/20260723_AaveV3Plasma_Onboard_PTsUSDe_22OCT2026_Oracle/AaveV3Plasma_Onboard_PTsUSDe_22OCT2026_Oracle_20260723.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGenericExecutor.sol';
+import {AaveV3PlasmaAssets} from 'aave-address-book/AaveV3Plasma.sol';
+import {AaveV3PayloadPlasma} from 'aave-helpers/src/v3-config-engine/AaveV3PayloadPlasma.sol';
+import {IAaveV3ConfigEngine} from 'aave-v3-origin/contracts/extensions/v3-config-engine/IAaveV3ConfigEngine.sol';
 
 /**
  * @title Onboard_PTsUSDe_22OCT2026_Oracle
@@ -9,6 +11,25 @@ import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGen
  * - Snapshot: https://gove.snapshot.placeholder
  * - Discussion: https://gov.discussion.placeholder
  */
-contract AaveV3Plasma_Onboard_PTsUSDe_22OCT2026_Oracle_20260723 is IProposalGenericExecutor {
-  function execute() external {}
+contract AaveV3Plasma_Onboard_PTsUSDe_22OCT2026_Oracle_20260723 is AaveV3PayloadPlasma {
+  // LlamaRisk PT oracle for PT-sUSDe-22OCT2026 (Plasma mainnet deployment)
+  address internal constant PT_sUSDE_22OCT2026_PT_ORACLE =
+    0x0000000000000000000000000000000000000000; // TODO: replace with deployed oracle address
+
+  function priceFeedsUpdates()
+    public
+    pure
+    override
+    returns (IAaveV3ConfigEngine.PriceFeedUpdate[] memory)
+  {
+    IAaveV3ConfigEngine.PriceFeedUpdate[]
+      memory priceFeedUpdates = new IAaveV3ConfigEngine.PriceFeedUpdate[](1);
+
+    priceFeedUpdates[0] = IAaveV3ConfigEngine.PriceFeedUpdate({
+      asset: AaveV3PlasmaAssets.PT_sUSDE_22OCT2026_UNDERLYING,
+      priceFeed: PT_sUSDE_22OCT2026_PT_ORACLE
+    });
+
+    return priceFeedUpdates;
+  }
 }
